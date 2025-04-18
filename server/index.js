60b92fd6a82367f62e5de5f884b2bd05dac62f53
Raj Verma <rajverma02604@gmail.com>
2025-04-18 19:48:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000/login",
+    origin: "https://chit-chatfrontend.vercel.app/login",
     credentials: true,
   },
 });
